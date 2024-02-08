3ad9519a9713f14dc857a810f12c134cab6a9c63
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-02-07 20:11:52 -0500

    comment out parts of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,16 @@
+
+// import {ReactComponent as GITHUB} from '../assets/icons/github.svg'
+// import {ReactComponent as LINKEDIN} from '../assets/icons/linkedin.svg'
+
+// const Footer =
+// [
+//     {
+//         link : 'https://github.com/jabrams513',
+//         icon : GITHUB
+//     },
+//     {
+//         link : 'https://www.linkedin.com/in/jonathan-abrams513/',
+//         icon : LINKEDIN
+//     }
+// ]
 export default function Footer() { return (<footer>Creative Solutions for Your Problems</footer>) }
