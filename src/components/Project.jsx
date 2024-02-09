bfcd523db08f7cc9f6e4fb8db53c15a8d4009932
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-02-09 16:46:29 -0500

    improved style

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
@@ -12,6 +12,12 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import * as projects from '../assets';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Oswald, sans-serif',
+  },
+});
 
 const CustomCard = styled(Card)(({ theme }) => ({
   backgroundColor: '#d3d3d3', // Grey background color
@@ -39,7 +45,7 @@ const CardsContainer = styled('div')({
 });
 
 export default function Project({ project }) {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState(true); // Expanded by default
 
   const { title, description, github, deployed, image, stack } = project;
 
@@ -48,54 +54,56 @@ export default function Project({ project }) {
   };
 
   return (
-    <CardsContainer>
-      <CustomCard sx={{ maxWidth: 345 }}>
-        <CardHeader
-          action={
-            <IconButton aria-label="settings">
-            </IconButton>
-          }
-          title={title}
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image={projects[image]}
-          alt={description}
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {stack}
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="github">
-            <a href={github} target="_blank" rel="noopener noreferrer">
-              <GitHubIcon />
-            </a>
-          </IconButton>
-          <IconButton aria-label="share">
-            <a href={deployed} target="_blank" rel="noopener noreferrer">
-              <ShareIcon />
-            </a>
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <ThemeProvider theme={theme}>
+      <CardsContainer>
+        <CustomCard sx={{ maxWidth: 345 }}>
+          <CardHeader
+            action={
+              <IconButton aria-label="settings">
+              </IconButton>
+            }
+            title={title}
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image={projects[image]}
+            alt={description}
+          />
           <CardContent>
-            <Typography paragraph>
+            <Typography variant="body2" color="text.secondary">
               {description}
             </Typography>
           </CardContent>
-        </Collapse>
-      </CustomCard>
-    </CardsContainer>
+          <CardActions disableSpacing>
+            <IconButton aria-label="github">
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                <GitHubIcon />
+              </a>
+            </IconButton>
+            <IconButton aria-label="share">
+              <a href={deployed} target="_blank" rel="noopener noreferrer">
+                <ShareIcon />
+              </a>
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>
+                {stack}
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </CustomCard>
+      </CardsContainer>
+    </ThemeProvider>
   );
 }
