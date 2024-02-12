c628ee7666ab69cfa5b6b7218dab5fd9a5de41cf
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-02-12 16:53:27 -0500

    something else

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import "./App.css"
 import Footer from './components/Footer';
-import Phoenix from '/Firebird.jpg'
 
 function App() {
-  const styles = { background: Phoenix }
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
       <Nav />
-      <main style = { styles }className="m-3">
+      <main className="m-3">
         <Outlet />
       </main>
       <Footer />
