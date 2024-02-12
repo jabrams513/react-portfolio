338f2dafb03aa896238bead6e0cf9eed227dabed
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-02-12 15:55:44 -0500

    typo

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -23,7 +23,7 @@ export default function Portfolio() {
         <p>Welcome to my portfolio, where pixels meet purpose and innovation transforms into code. Below are some selected projects that showcase my skills, creativity, and commitment to delivering exceptional web solutions.</p>
         <p>These are just a few examples of my work, each representing a unique challenge and an opportunity to push the boundaries of web development. Click on each project to explore further details and see the results firsthand.</p>
         <p>Interested in learning more about my skills and approach? Visit my resume for a comprehensive overview of my professional background.</p>
-        <p>If you have a project in mind or would like to discuss potential collaborations, feel free to visit my resume and contact pages for more information on how to get in touch. Let's bring your ideas to life</p>
+        <p>If you have a project in mind or would like to discuss potential collaborations, feel free to visit my resume and contact pages for more information on how to get in touch. Let's bring your ideas to life.</p>
       </div>
     </div>
   );
